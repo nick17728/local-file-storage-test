4425a0951f2ce82164fb2aa2b97a4189dc7fef2b
Nikhil Bellavi <nickbellavi@gmail.com>
2023-10-14 11:47:23 +0530

    test files

---
+++ sdui/wormhole.jsx
@@ -0,0 +1,28 @@
+import { jsx as _jsx } from "react/jsx-runtime";
+import { jsxs as _jsxs } from "react/jsx-runtime";
+import * as React from "react";
+import { Animated, Alert, TouchableOpacity } from "react-native";
+
+function CustomButton() {
+  return /*#__PURE__*/ _jsx(TouchableOpacity, {
+    onPress: () => Alert.alert("Hello!"),
+    children: /*#__PURE__*/ _jsx(Animated.Text, {
+      children: "Click here!",
+    }),
+  });
+}
+export default function wormhole() {
+  const message = React.useMemo(() => "Hello, world!", []);
+  return /*#__PURE__*/ _jsxs(Animated.View, {
+    style: {
+      flex: 1,
+      backgroundColor: "red",
+    },
+    children: [
+      /*#__PURE__*/ _jsx(Animated.Text, {
+        children: message,
+      }),
+      /*#__PURE__*/ _jsx(CustomButton, {}),
+    ],
+  });
+}
